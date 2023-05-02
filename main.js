47e5c915cd06e9ce2e0f6453d69b2c12c84508ac
RogerAcevedo <Rogeracevedocreate@gmail.com>
2023-05-02 16:47:22 -0700

    chess board update

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const king = document.querySelector('.chess-piece')
 const squares = document.querySelectorAll('.square')
+const infoDisplay = document.querySelector('#info')
 
-king.addEventListener('dragstart', dragStart) 
+king.addEventListener('dragstart', dragStart)
+king.addEventListener('drag', dragging) 
 
 squares.forEach(square => {
     square.addEventListener('dragover', dragOver)
     square.addEventListener('drop', dragDrop)
+    square.addEventListener('dragenter', dragEnter )
+    square.addEventListener('dragleave', dragLeave)
+    square.addEventListener('dragend', dragEnd)
 })
 
 let beingDragged
+
+function dragging() {
+    infoDisplay.textContent = 'YOU ARE DRAGGING A ' + beingDragged.id
+}
 
 function dragStart(e) {
     beingDragged = e.target
@@ -19,5 +28,21 @@ function dragOver(e) {
 }
 
 function dragDrop(e) {
-    console.log(e.target);
+    // console.log(e.target);
+    e.target.append(beingDragged)
+    e.target.classList.remove('highlight')
+}
+
+function dragEnter (e) {
+    e.target.classList.add('highlight')
+}
+
+function dragLeave (e) {
+    e.target.classList.remove('highlight')
+}
+
+function dragEnd(e) {
+    e.target.classList.add('target')
+    setTimeout(() => e.target.classList.remove('target', 100))
+    infoDisplay.textContent = ''
 }
